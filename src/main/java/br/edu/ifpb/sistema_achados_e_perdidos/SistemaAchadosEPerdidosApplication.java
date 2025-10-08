61f66ae51e377482d80fc3e1e3a38d748e9cf379
Carlos Miguel <miguel.barbosa@academico.ifpb.edu.br>
2025-10-07 21:40:21 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ifpb/sistema_achados_e_perdidos/SistemaAchadosEPerdidosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.sistema_achados_e_perdidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAchadosEPerdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAchadosEPerdidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/sistema_achados_e_perdidos/SistemaAchadosEPerdidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.sistema_achados_e_perdidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAchadosEPerdidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
